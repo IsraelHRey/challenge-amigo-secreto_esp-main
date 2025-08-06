48892b72c7f7d59c0c9e0a3607dfaf85cec3ca02
Israel Hernandez <israel.hdzrys66@gmail.com>
2025-08-06 02:30:33 -0600

    se realizo funcion para ver listas y funcion aleatorio

--- app.js
+++ app.js
@@ -1,16 +1,41 @@
 let amigosecreto = [];
-
-function nombreAmigos () {
+                                                                    
+function nombreAmigos () {                                           //document es el cuerpo del texto
   let ingresoDeNombre = document.getElementById("amigo")
-  let nombre = ingresoDeNombre.value
+  let nombre = ingresoDeNombre.value                                //value es para que sea igual al valor, no solo string
 
     if (nombre === "") {
         alert ("Por favor, inserte un nombre.")
     return;
     } 
-    amigosecreto.push(nombre)
+    amigosecreto.push(nombre)               //.push guarda las variables en la lista
 ingresoDeNombre.value = "";
 
 }
 
+function mostrarAmigos (){
+let listaamigos = document.getElementById ("listaAmigos")
+lista.innerHTML = ""
 
+    for (let i=0; i<amigosecreto.length; i++){
+      let etiquetaLi = document.createElement("li");
+      etiquetaLi.textContent =  amigosecreto[i]
+     listaamigos.appendChild(etiquetaLi)
+
+      
+    }
+
+}
+
+
+function amigoAleatorio(){
+  if (amigosecreto.length === 0) {
+    return
+  }
+
+  let indiceAlatorio = Math.floor(Math.random()* amigosecreto.length)
+  let amigoSorteado = amigosecreto[indiceAlatorio];
+
+  let resultado = document.getElementById(resultado).innerHTML = amigoSorteado;
+
+}
