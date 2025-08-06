420b8e3ab580ec81a5185fb06667de86eea360cb
Israel Hernandez <israel.hdzrys66@gmail.com>
2025-08-05 20:12:59 -0600

    se realizo funcion agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-let amigosecreto = []
+let amigosecreto = [];
+
+function nombreAmigos () {
+  let ingresoDeNombre = document.getElementById("amigo")
+  let nombre = ingresoDeNombre.value
+
+    if (nombre === "") {
+        alert ("Por favor, inserte un nombre.")
+    return;
+    } 
+    amigosecreto.push(nombre)
+ingresoDeNombre.value = "";
+
+}
+
+
